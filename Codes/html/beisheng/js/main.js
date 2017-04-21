732a1e4e4b32fa5f0e7d05c7a671522d2e437fc8
gongthub <1056685839@qq.com>
2017-04-21 09:56:34 +0800

    首页视频基本完成

--- Codes/html/beisheng/js/main.js
+++ Codes/html/beisheng/js/main.js
@@ -4,75 +4,26 @@
 // wow.js
 new WOW().init();
 // $(function () {})
-$(function() {
-	wbannertop();
-	indexProjectShow();
-	$(".div-project-center-btn-img").mouseover(function() {
-		initIndexProjectBtnMous(this);
-	});
+$(function () {
+    wbannertop();
 })
 //resize
-$(window).resize(function() {
-	wbannertop();
+$(window).resize(function () {
+    wbannertop();
 })
 // index js
 //nav js
-$('.navbar-nav>li>a').click(function() {
-	$('.navbar-nav>li').removeClass('active');
-	$(this).parent('li').addClass('active');
+$('.navbar-nav>li>a').click(function () {
+    $('.navbar-nav>li').removeClass('active');
+    $(this).parent('li').addClass('active');
 })
 //轮播 js
 $('.carousel').carousel({
-	interval: false
+    interval: false
 })
 // 固定导航后的banner距离
 function wbannertop() {
-	//var wnavhigh = $('.w-navwrap').height();
-	//$('.w-indexbanner').css('margin-top',wnavhigh);
+    //var wnavhigh = $('.w-navwrap').height();
+    //$('.w-indexbanner').css('margin-top',wnavhigh);
 }
 
-function initIndexProjectBtnMous(e) {
-	if(!$(e).hasClass("div-project-center-btn-img-active")) {
-		var showid = $(e).attr("showid");
-		var showdiv = $(".div-project-center-btn-img");
-		if(showdiv != undefined && showdiv.length > 0) {
-			for(var i = 0; i < showdiv.length; i++) {
-				if($(showdiv[i]).attr("showid") == showid) {
-					$(showdiv[i]).addClass("div-project-center-btn-img-active");
-				} else {
-					$(showdiv[i]).removeClass("div-project-center-btn-img-active");
-				}
-			}
-		}
-		indexProjectShow();
-	}
-
-}
-
-function initIndexProjectBtn() {
-	var showIdT = 0;
-	var showdiv = $(".div-project-center-btn-img");
-	if(showdiv != undefined && showdiv.length > 0) {
-		for(var i = 0; i < showdiv.length; i++) {
-			if($(showdiv[i]).hasClass("div-project-center-btn-img-active")) { 
-				showIdT = $(showdiv[i]).attr("showid");
-			}
-		}
-	}
-	return showIdT;
-}
-
-function indexProjectShow() {
-	showid = initIndexProjectBtn();
-	var showdiv = $("#div-project-center-show").find(".div-project-center-text");
-	if(showdiv != undefined && showdiv.length > 0) {
-		for(var i = 0; i < showdiv.length; i++) {
-			var showIdT = $(showdiv[i]).attr("showid");
-			if(showIdT == showid) {
-				$(showdiv[i]).attr("style", "display:block");
-			} else {
-				$(showdiv[i]).attr("style", "display:none");
-			}
-		}
-	}
-}
